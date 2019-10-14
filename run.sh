0f5eaba283739dca4915b91d05ef8e898d604307
wangyang <1749748955@qq.com>
2019-10-14 15:19:18 +0800

    添加jenkins脚本文件

---
+++ run.sh
@@ -0,0 +1,13 @@
+chmod +x gradlew
+./gradlew build
+echo "-----------------------"
+pid=`lsof -i:8084 | awk '{print $2}' | tail -n 1`
+if [  -n "$pid" ];then ~/springboot_demo/
+   kill -9 $pid
+fi
+rm -rf  ~/springboot_demo
+mkdir  ~/springboot_demo
+cp build/libs/gradle-springboot.jar  ~/springboot_demo
+cd  ~/springboot_demo
+export BUILD_ID=dontKillMe
+nohup java -jar gradle-springboot.jar >>log8084.txt  &
